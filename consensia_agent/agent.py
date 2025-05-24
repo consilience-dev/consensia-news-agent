62861c60d7345b352d8fa8dede9d388d79372917
consilience <dev.consilience@gmail.com>
2025-05-24 14:03:51 -0700

    Refactor agent.py: Use LiteLlm for agents and update sub-agent integration

--- consensia_agent/agent.py
+++ consensia_agent/agent.py
@@ -6,18 +6,22 @@ from google.adk.agents import SequentialAgent, ParallelAgent, LlmAgent
 from google.adk.tools.agent_tool import AgentTool
 from .tools import get_related_news_gnews
 from .tools import get_related_news
+from google.adk.models.lite_llm import LiteLlm
 
 compare_stories_agent = Agent(
     name="compare_stories_agent",
-    model="gemini-2.5-flash-preview-05-20", # Can be a string for Gemini or a LiteLlm object
-    description="Compares a list of related news stories based on topic and date.",
+    #model="gemini-2.5-flash-preview-05-20", # Can be a string for Gemini or a LiteLlm object
+    model=LiteLlm("claude-sonnet-4-20250514"),
+    description="Compares a list of related news stories that are organized on topic and date.",
     instruction=COMPARE_STORIES_AGENT_PROMPT_v1,
 )
 
 root_agent = Agent(
     name="news_search_agent_v1",
-    model="gemini-2.0-flash", # Can be a string for Gemini or a LiteLlm object
+    #model="gemini-2.0-flash", # Can be a string for Gemini or a LiteLlm object
+    model=LiteLlm("claude-sonnet-4-20250514"),
     description="Looks up related news stories based on topic and date.",
     instruction=NEWS_SEARCH_AGENT_PROMPT_v1,
-    tools=[get_related_news_gnews, AgentTool(compare_stories_agent)], # Pass the function directly
+    sub_agents=[compare_stories_agent],
+    tools=[get_related_news_gnews], # Pass the function directly
 )
